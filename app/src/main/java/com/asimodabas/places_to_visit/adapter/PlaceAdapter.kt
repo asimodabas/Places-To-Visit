86f6c3370751bd8fd6c5e29c8b37c7e15013cb12
Hacı Asım Odabaş <71982171+asimodabas@users.noreply.github.com>
2021-12-23 10:09:04 +0300

    PlaceAdapter

---
+++ app/src/main/java/com/asimodabas/places_to_visit/adapter/PlaceAdapter.kt
@@ -0,0 +1,35 @@
+package com.asimodabas.places_to_visit.adapter
+
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.Placeholder
+import androidx.recyclerview.widget.RecyclerView
+import com.asimodabas.places_to_visit.databinding.RecyclerRowBinding
+import com.asimodabas.places_to_visit.model.Place
+import com.asimodabas.places_to_visit.view.MapsActivity
+
+class PlaceAdapter(val placeList: List<Place>) : RecyclerView.Adapter<PlaceAdapter.PlaceHolder>() {
+
+    class PlaceHolder(val recyclerRowBinding: RecyclerRowBinding) :
+        RecyclerView.ViewHolder(recyclerRowBinding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlaceHolder {
+        val recyclerRowBinding = RecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return PlaceHolder(recyclerRowBinding)
+    }
+
+    override fun onBindViewHolder(holder: PlaceHolder, position: Int) {
+        holder.recyclerRowBinding.recyclerViewTextView.text=placeList.get(position).name
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context,MapsActivity::class.java)
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return placeList.size
+    }
+}
--- app/src/main/java/com/asimodabas/places_to_visit/view/MainActivity.kt
+++ app/src/main/java/com/asimodabas/places_to_visit/view/MainActivity.kt
@@ -6,22 +6,55 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
 import com.asimodabas.places_to_visit.R
+import com.asimodabas.places_to_visit.adapter.PlaceAdapter
+import com.asimodabas.places_to_visit.databinding.ActivityMainBinding
+import com.asimodabas.places_to_visit.model.Place
+import com.asimodabas.places_to_visit.roomdb.PlaceDao
+import com.asimodabas.places_to_visit.roomdb.PlaceDatabase
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val compositeDisposable = CompositeDisposable()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val db =
+            Room.databaseBuilder(applicationContext, PlaceDatabase::class.java, "Places").build()
+        val placeDao = db.placeDao()
+
+        compositeDisposable.add(
+            placeDao.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handlerResponse)
+        )
+    }
+
+    private fun handlerResponse(placeList: List<Place>) {
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = PlaceAdapter(placeList)
+        binding.recyclerView.adapter = adapter
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.place_menu,menu)
+        menuInflater.inflate(R.menu.place_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.add_place){
-            Toast.makeText(this,"Add Place",Toast.LENGTH_SHORT).show()
+        if (item.itemId == R.id.add_place) {
+            Toast.makeText(this, "Add Place", Toast.LENGTH_SHORT).show()
             val intent = Intent(this, MapsActivity::class.java)
             startActivity(intent)
         }
